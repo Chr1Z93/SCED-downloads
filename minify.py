dabcde95b0be0e0ab7b5ffa35a7fc92d73243920
Chr1Z93 <puls.christian@gmail.com>
2024-11-24 22:27:46 +0100

    updated minify script

--- minify.py
+++ minify.py
@@ -6,11 +6,11 @@ OUTPUT_DIR = "./.build"
 def minify_json_files(directory):
     directory_path = Path(directory)
     for json_file in directory_path.glob("*.json"):
-        with open(json_file, "r") as f:
+        with open(json_file, "r", encoding="utf-8") as f:
             data = json.load(f)
 
-        with open(json_file, "w") as f:
-            json.dump(data, f, separators=(",", ":"))
+        with open(json_file, "w", encoding="utf-8") as f:
+            json.dump(data, f, separators=(",", ":"), ensure_ascii=False)
 
 if __name__ == "__main__":
     minify_json_files(OUTPUT_DIR)
